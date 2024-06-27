41f9d0150826c5cd02e19febf628cdfca0199286
YliaVaininlovich <ylia.v@tut.by>
2024-06-27 07:10:23 +0300

    feat: fix header

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import logotip from "../../image/cornflower.png";
+// import logotip from "../../image/cornflower.png";
 import { LeftPanel } from "../LeftPanel/LeftPanel";
 
 import './Header.scss'
@@ -34,26 +34,25 @@ export function Header() {
   return (
     <div className="header_container">
       <header className="header">
-        <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-        <div className="header__logotip"><img src={logotip} alt="logotip" /></div>
-        <div className="header__text">Архив музея Александрии</div>
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+          <div className="header__text">Архив музея Александрии</div>
         </div>
-        
-        
-        <div className="burger" onClick={handleBurgerClick} style={{cursor: "pointer"}}>
+
+
+        <div className="burger" onClick={handleBurgerClick} style={{ cursor: "pointer" }}>
           <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-            <g id="SVGRepo_iconCarrier"> <g clip-path="url(#clip0_429_11066)"> <path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="#5A7CF1" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/> </g> <defs> <clipPath id="clip0_429_11066"> <rect width="24" height="24" fill="white" transform="translate(0 0.000915527)"/> </clipPath> </defs> </g>
+            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+            <g id="SVGRepo_iconCarrier"> <g clip-path="url(#clip0_429_11066)"> <path d="M3 6.00092H21M3 12.0009H21M3 18.0009H21" stroke="#5A7CF1" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" /> </g> <defs> <clipPath id="clip0_429_11066"> <rect width="24" height="24" fill="white" transform="translate(0 0.000915527)" /> </clipPath> </defs> </g>
           </svg>
         </div>
         {isPanelOpen && (
           <div className={`panel ${isPanelOpen ? 'open' : ''}`}>
-          <div className="close" onClick={handleCloseClick}>
+            <div className="close" onClick={handleCloseClick}>
               <div className="line line1"></div>
               <div className="line line2"></div>
             </div>
-          <LeftPanel activeButton={activeButton} />
+            <LeftPanel activeButton={activeButton} />
           </div>
         )}
 
